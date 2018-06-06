e06d9b1626215bdadb4ee2112cff17ec5358f019
ChingyuChen123 <che15022@byui.edu>
2018-06-06 12:15:48 -0600

    display function

--- app/src/main/java/com/example/wchang/team5_project/MainActivity.java
+++ app/src/main/java/com/example/wchang/team5_project/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.wchang.team5_project;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +13,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
     // This is Brysen's comment
+
+    public void display(View view) {
+        String s = "testing";
+    }
 }
