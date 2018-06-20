5cd82337562fb27bd5213e6f3297e179d0d2bb1e
Brysen Hawkes <bdhawkes@gmail.com>
2018-06-20 12:17:39 -0600

    Adding state machine functions to main activity and Shared Preferences code(not finished).

--- app/src/main/java/com/example/wchang/team5_project/MainActivity.java
+++ app/src/main/java/com/example/wchang/team5_project/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.wchang.team5_project;
 
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,7 +11,42 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        readSharedPreferances();
     }
-    // This is Brysen's comment
+
+    protected void onPause() {
+        super.onPause();
+        sriteSharedPreferences();
+    }
+
+    protected void onStart(){
+        super.onStart();
+    }
+
+    protected void onResume(){
+        super.onResume();
+    }
+
+
+
+    protected void onStop(){
+        super.onStop();
+    }
+
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    private void writeShareddPreferences(){
+        SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(this);
+        SharedPreferences.Editor editor = sp.edit();
+        editor.putString(SP;
+        editor.commit();
+    }
+    private void readSharedPreferences(){
+        SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(this);
+        String ... = sp.getString(...);
+        ...
+        }
 }
 //This is Darren's comment 2
