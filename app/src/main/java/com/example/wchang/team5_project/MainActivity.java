ba56f3491a2cf3700693eb47b03fdc60a60a6a95
ChingyuChen123 <che15022@byui.edu>
2018-06-20 18:38:17 -0600

    Put SharedPreferences Functions into Fragment class

--- app/src/main/java/com/example/wchang/team5_project/MainActivity.java
+++ app/src/main/java/com/example/wchang/team5_project/MainActivity.java
@@ -15,14 +15,43 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    private static String SP_USER_STATUS = "userstatus";
-    private EditText editName;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
+    }
+
+    protected void onPause() {
+        super.onPause();
+    }
+
+    protected void onStart(){
+        super.onStart();
+    }
+
+    protected void onResume(){
+        super.onResume();
+    }
+
+    protected void onStop(){
+        super.onStop();
+    }
+
+    protected void onDestroy() {
+        super.onDestroy();
+    }
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
             Fragment selectedFragment = null;
 
             switch (item.getItemId()) {
@@ -44,56 +73,4 @@ public class MainActivity extends AppCompatActivity {
             return true;
         }
     };
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        editName = findViewById(R.id.name_text);
-
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
-        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
-        readSharedPreferences();
-    }
-
-    protected void onPause() {
-        super.onPause();
-        writeSharedPreferences();
-    }
-
-    protected void onStart(){
-        super.onStart();
-    }
-
-    protected void onResume(){
-        super.onResume();
-
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
-        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
-    }
-
-
-
-    protected void onStop(){
-        super.onStop();
-    }
-
-    protected void onDestroy() {
-        super.onDestroy();
-    }
-
-    private void writeSharedPreferences(){
-        SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(this);
-        SharedPreferences.Editor editor = sp.edit();
-        editor.putString(SP_USER_STATUS, editName.getText().toString());
-        editor.commit();
-    }
-    private void readSharedPreferences(){
-        SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(this);
-        String name = sp.getString(SP_USER_STATUS, "");
-        editName.setText(name);
-        }
 }
