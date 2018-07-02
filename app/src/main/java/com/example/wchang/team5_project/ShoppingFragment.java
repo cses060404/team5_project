bd4af22220c5368f65d57e76ccb564b8f14b6ddb
Wen Yao Chang <cha16034@byui.edu>
2018-07-02 12:31:24 -0600

    shoppinglist update

--- app/src/main/java/com/example/wchang/team5_project/ShoppingFragment.java
+++ app/src/main/java/com/example/wchang/team5_project/ShoppingFragment.java
@@ -1,18 +1,46 @@
 package com.example.wchang.team5_project;
 
 import android.os.Bundle;
-        import android.support.annotation.NonNull;
-        import android.support.annotation.Nullable;
-        import android.support.v4.app.Fragment;
-        import android.view.LayoutInflater;
-        import android.view.View;
-        import android.view.ViewGroup;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class ShoppingFragment extends Fragment {
+
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_shopping, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_shopping, container, false);
+
+        mRecyclerView = (RecyclerView) view.findViewById(R.id.shoppingView);
+
+        // use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        mRecyclerView.setHasFixedSize(true);
+
+        // use a linear layout manager
+        mLayoutManager = new LinearLayoutManager(getContext());
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        // specify an adapter (see also next example)
+        mAdapter = new MyAdapter(myDataset);
+        mRecyclerView.setAdapter(mAdapter);
+
+
+
+        return view;
     }
+
+
 }
