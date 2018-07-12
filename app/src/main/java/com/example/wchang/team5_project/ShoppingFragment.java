05226f99d6497cc70e6eeb7ab8133a500f8fbcb8
Wen Yao Chang <cha16034@byui.edu>
2018-07-12 12:53:16 -0600

    shoppinglist update

--- app/src/main/java/com/example/wchang/team5_project/ShoppingFragment.java
+++ app/src/main/java/com/example/wchang/team5_project/ShoppingFragment.java
@@ -5,6 +5,7 @@ package com.example.wchang.team5_project;
         import android.support.annotation.NonNull;
         import android.support.annotation.Nullable;
         import android.support.v4.app.Fragment;
+        import android.support.v7.widget.LinearLayoutManager;
         import android.support.v7.widget.RecyclerView;
         import android.view.LayoutInflater;
         import android.view.View;
@@ -18,7 +19,8 @@ package com.example.wchang.team5_project;
 public class ShoppingFragment extends Fragment {
 
     private RecyclerView recyclerView;
-    private Vector<Recipe> recipes;
+ //   private Vector<Recipe> recipes;
+    private ShoppingAdapter adapter;
 
     @Nullable
     @Override
@@ -27,8 +29,25 @@ public class ShoppingFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_shopping, container, false);
 
         recyclerView = (RecyclerView) view.findViewById(R.id.shoppingView);
+
+        adapter = new ShoppingAdapter(getActivity(), getData());
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
         return view;
     }
 
+    public static Vector<Recipe> getData(){
+        Vector<Recipe> data = new Vector<>();
+        int[] icons = {R.drawable.apples,R.drawable.apples};
+        String[] titles = {"haha", "CC"};
+        for (int i = 0; i < titles.length && i < icons.length; i++ )
+        {
+            Recipe current = new Recipe();
+            current.name = titles[i];
+            data.add(current);
+        }
+        return data;
+    }
 
 }
