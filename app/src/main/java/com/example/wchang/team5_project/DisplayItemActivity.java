cbbdc8d571cd2947920c8c6d2358b4f822b3f0a1
darrenjk <ker16005@byui.edu>
2018-07-13 12:48:23 -0600

    Merge branch 'master' of C:\Users\darre\AndroidStudioProjects\team5_project with conflicts.

--- app/src/main/java/com/example/wchang/team5_project/DisplayItemActivity.java
+++ app/src/main/java/com/example/wchang/team5_project/DisplayItemActivity.java
@@ -1,11 +1,7 @@
 package com.example.wchang.team5_project;
 
-import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.os.Bundle;
 
 public class DisplayItemActivity extends AppCompatActivity {
 
@@ -13,9 +9,5 @@ public class DisplayItemActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_item);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
     }
-
 }
