917065acef4164eb758d26d60a3cebe643416954
Brysen Hawkes <bdhawkes@gmail.com>
2018-07-12 19:56:26 -0600

    Adding the makeList function to the controller. This takes a Vector<Recipe> as a parameter and returns a Vector<FoodItem>

--- app/src/main/java/com/example/wchang/team5_project/Controller.java
+++ app/src/main/java/com/example/wchang/team5_project/Controller.java
@@ -46,6 +46,29 @@ public class Controller implements Serializable {
         model.addRecipe(newRecipe);
     }
 
+    public Vector<FoodItem> makeList(Vector<Recipe> usingRecipe){
+        Vector<FoodItem> needed = new Vector<>();
+        for(int i = 0; i < usingRecipe.size(); i++){
+            for(int j = 0; j < usingRecipe.get(i).getIngredients().size(); j++) {
+                for(int k = 0; k < model.pantry.size(); k++) {
+                // j is the recipe FoodItem index
+                // k is the pantry FoodItem index
+                // compare all J's to K's. if not found add to needed list
+                    if (model.pantry.get(k).name.equals(usingRecipe.get(i).getIngredients().get(j).name) ) {
+                        //create a new FoodItem with the needed quantity
+                        FoodItem newFoodItem = new FoodItem(model.pantry.get(k).name, (usingRecipe.get(i).getIngredients().get(j).quantity - model.pantry.get(k).quantity), model.pantry.get(k).unit);
+                        if(newFoodItem.quantity > 0)
+                            needed.add(newFoodItem);
+                    } else {
+                        FoodItem newFoodItem = new FoodItem(usingRecipe.get(i).getIngredients().get(j).name, usingRecipe.get(i).getIngredients().get(j).quantity, usingRecipe.get(i).getIngredients().get(j).unit);
+                        needed.add(newFoodItem);
+                    }
+                }
+            }
+        }
+        return needed;
+    }
+
     public Vector<Recipe> getRecipes() {return model.recipes;}
 
     public Vector<FoodItem> getPantry() {return model.pantry;}
