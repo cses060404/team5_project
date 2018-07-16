5ee6f83bbf4a691de8a1fe8cbcbfbe4410a526db
darrenjk <ker16005@byui.edu>
2018-07-16 11:39:51 -0600

    update

--- app/src/main/java/com/example/wchang/team5_project/DisplayItemActivity.java
+++ app/src/main/java/com/example/wchang/team5_project/DisplayItemActivity.java
@@ -2,12 +2,22 @@ package com.example.wchang.team5_project;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class DisplayItemActivity extends AppCompatActivity {
+FoodItem oldFoodItem;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_item);
+        oldFoodItem = new FoodItem("placeholder",1,"na");
+    }
+
+    public void deleteBtn(View view) {
+        MainActivity.controller.deleteItem(oldFoodItem);
+        Toast.makeText(this, "Item Deleted!", Toast.LENGTH_LONG).show();
+        finish();
     }
 }
